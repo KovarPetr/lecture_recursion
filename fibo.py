70996c3c27e59616871534c1cb5ecf9e5141f815
Kovář Petr (257014) <257014@vutbr.cz>
2024-04-22 12:08:28 +0200

    Implementace algoritmu

--- fibo.py
+++ fibo.py
@@ -1,5 +1,14 @@
+def recursive_nth_fibo(n):
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
+    print(recursive_nth_fibo(12))
     pass
 
 
